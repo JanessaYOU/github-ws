6baedce8c0c678fed5e8aabc693a5066fe25b736
JanessaYou <yjj23890219@gmail.com>
2024-01-21 22:43:04 +0800

    commit

---
+++ multiply.py
@@ -0,0 +1,14 @@
+def multiply(a: int, b: int) -> int:
+    a * b
+
+if __name__ == "__main__":
+    print("Please input the calculation you want: ")
+    mode = input()
+    print("Input the first number: ")
+    a = int(input())
+    print("Input the second number: ")
+    b = int(input())
+    if mode == "add":
+        print(f"The sum of {a} and {b} is: {add(a, b)}")
+    elif mode == "multiply":
+        print(f"The multiplication of {a} and {b} is: {multiply(a, b)} ")
